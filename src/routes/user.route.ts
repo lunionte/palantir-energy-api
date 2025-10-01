97bf34de40f205c89e7b06edeb2293fe6bbd845f
italo <italoteles399@gmail.com>
2025-10-01 10:34:08 -0300

    adicionado rota de usuarios

---
+++ src/routes/user.route.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { authmiddleware } from "../middlewares/auth.middleware";
+import { UserController } from "../controllers/user.controller";
+
+export const userRoute = Router();
+
+userRoute.get("/", authmiddleware, UserController.getAll);
