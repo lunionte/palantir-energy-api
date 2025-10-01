1bcd3dbf7a04821274ce84059a7c5845c015bded
italo <italoteles399@gmail.com>
2025-10-01 10:34:19 -0300

    adicionado rota de autenticação

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controller";
+import { authmiddleware } from "../middlewares/auth.middleware";
+import { celebrate, Segments } from "celebrate";
+import { loginSchema, newUserSchema } from "../models/user.model";
+
+export const authRoute = Router();
+
+authRoute.post(
+    "/register",
+    authmiddleware,
+    celebrate({ [Segments.BODY]: newUserSchema }),
+    AuthController.cadastrar
+);
+authRoute.post("/login", celebrate({ [Segments.BODY]: loginSchema }), AuthController.logar);
