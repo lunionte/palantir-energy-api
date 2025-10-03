d6e976b84d9cd2b71d38af3ec43e33bb138a72fe
italo <italoteles399@gmail.com>
2025-10-03 12:13:29 -0300

    atualizado schemas do joi/celebrate

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,17 +7,28 @@ export enum StatusFuncionario {
 }
 
 export const newUserSchema = Joi.object().keys({
-    nome: Joi.string().max(50).trim(),
-    email: Joi.string().email().trim(),
-    password: Joi.string().max(50).trim(),
-    status: Joi.string().optional(),
+    nome: Joi.string().max(50).trim().required(),
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().min(6).max(50).trim().required(),
+});
+
+export const updateUserSchema = Joi.object().keys({
+    nome: Joi.string().min(1).max(50).trim().optional(),
+    email: Joi.string().email().optional(),
+    password: Joi.string().min(6).max(50).optional(),
+    status: Joi.string()
+        .valid(...Object.values(StatusFuncionario))
+        .optional(),
 });
 
 export const loginSchema = Joi.object().keys({
-    email: Joi.string().email().trim(),
-    password: Joi.string().max(50).trim(),
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().max(50).trim().required(),
 });
 
+export const newPasswordSchema = Joi.object().keys({
+    newPassword: Joi.string().min(6).max(50).required(),
+});
 export const queryParamsSchema = Joi.object().keys({
     status: Joi.string()
         .uppercase()
