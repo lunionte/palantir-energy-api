87322f5d1882d844bb40602346969972309ee2b0
italo <italoteles399@gmail.com>
2025-10-03 12:14:04 -0300

    adicionado rota de atualizar senha

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { authmiddleware } from "../middlewares/auth.middleware";
 import { celebrate, Segments } from "celebrate";
-import { loginSchema, newUserSchema } from "../models/user.model";
+import { loginSchema, newPasswordSchema, newUserSchema } from "../models/user.model";
 
 export const authRoute = Router();
 
@@ -13,3 +13,9 @@ authRoute.post(
     AuthController.cadastrar
 );
 authRoute.post("/login", celebrate({ [Segments.BODY]: loginSchema }), AuthController.logar);
+authRoute.post(
+    "/update-password",
+    celebrate({ [Segments.BODY]: newPasswordSchema }),
+    authmiddleware,
+    AuthController.updatePassword
+);
