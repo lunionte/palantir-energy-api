414b0ed3f1471382b530125ce4a74e5ba20e052a
italo <italoteles399@gmail.com>
2025-10-03 09:01:16 -0300

    adicionado validação de query

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { authmiddleware } from "../middlewares/auth.middleware";
 import { UserController } from "../controllers/user.controller";
+import { celebrate, Segments } from "celebrate";
+import { queryParamsSchema } from "../models/user.model";
 
 export const userRoute = Router();
 
-userRoute.get("/", authmiddleware, UserController.getAll);
+userRoute.get(
+    "/",
+    celebrate({ [Segments.QUERY]: queryParamsSchema }),
+    authmiddleware,
+    UserController.getAll
+);
 userRoute.get("/:id", authmiddleware, UserController.getById);
